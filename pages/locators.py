7f058c8c38d7567d2ccd21c157c7abc95bad310e
dima-marchuk <dmytromarchukqa@gmail.com>
2022-06-18 17:27:27 +0300

    added product page test

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    BOOK_TITLE_IN_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    BOOK_PRICE_IN_MESSAGE = (By.CSS_SELECTOR, ".in div > p:nth-child(1) > strong")
+    BOOK_TITLE = (By.CSS_SELECTOR, ".product_main > h1")
+    BOOK_PRICE = (By.CSS_SELECTOR, "p.price_color")
---
+++ pages/product_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        basket_button.click()
+
+    def product_should_have_correct_name(self):
+        book_title_in_message = self.browser.find_element(*ProductPageLocators.BOOK_TITLE_IN_MESSAGE)
+        book_title = self.browser.find_element(*ProductPageLocators.BOOK_TITLE)
+        assert book_title_in_message.text == book_title.text, "incorrect title"
+
+    def product_should_have_correct_price(self):
+        book_price_in_message = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_IN_MESSAGE)
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE)
+        assert book_price_in_message.text == book_price.text, "incorrect price"
+
