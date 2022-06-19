a441a4340b5219a1820377f0f1737ed5077a3516
dima-marchuk <dmytromarchukqa@gmail.com>
2022-06-19 17:26:22 +0300

    removed page objects from the main_page and added them to the base_page, added 2 locators to the locators file, added 2 tests to the test_product_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,6 +7,9 @@ import math
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from .locators import BasePageLocators
+
+
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -53,3 +56,10 @@ class BasePage:
             return False
 
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- pages/locators.py
+++ pages/locators.py
@@ -17,3 +17,8 @@ class ProductPageLocators:
     BOOK_TITLE = (By.CSS_SELECTOR, ".product_main > h1")
     BOOK_PRICE = (By.CSS_SELECTOR, "p.price_color")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div:nth-child(1)")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
