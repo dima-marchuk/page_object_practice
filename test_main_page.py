e06b8e964d438854f37b11046d5038e3857db61a
dima-marchuk <dmytromarchukqa@gmail.com>
2022-07-05 13:59:01 +0300

    Tiny fixes

--- test_main_page.py
+++ test_main_page.py
@@ -11,9 +11,9 @@ from pages.basket_page import BasketPage
 class TestLoginFromMainPage:
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com"
-        page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()  # открываем страницу
-        page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+        page = MainPage(browser, link)  # ініціалізуєм Page Object, передаємо в конструктор драйвера екземпляр драйвера і url адресу
+        page.open()  # відкриваємо сторінку
+        page.go_to_login_page()  # виконуємо метод сторінки — переходимо на сторінку логіна
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
